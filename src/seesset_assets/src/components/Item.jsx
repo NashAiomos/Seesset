c5f8ad3cda2fa1842112a407f94ba22b43695b0b
Neutronstarpro <neutronstarpro@NeutronStarPRO.localdomain>
2022-08-27 15:25:55 +0800

    first commit

---
+++ src/seesset_assets/src/components/Item.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from "react";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { idlFactory as tokenIdlFactory } from "../../../declarations/token";
+import { Principal } from "@dfinity/principal";
+import { seesset } from "../../../declarations/seesset";
+import Button from "./Button";
+import CURRENT_USER_ID from "../index";
+import PriceLabel from "./PriceLabel";
+
+function Item(props) {
+  const [name, setName] = useState();
+  const [owner, setOwner] = useState();
+  const [image, setImage] = useState();
+  const [button, setButton] = useState();
+  const [priceInput, setPriceInput] = useState();
+  const [loaderHidden, setLoaderHidden] = useState(true);
+  const [blur, setBlur] = useState();
+  const [sellStatus, setSellStatus] = useState("");
+  const [priceLabel, setPriceLabel] = useState();
+  const [shouldDisplay, setDisplay] = useState(true);
+
+  const id = props.id;
+
+  const localHost = "http://localhost:8080/";
+  const agent = new HttpAgent({ host: localHost });
+
+  //TODO: When deploy live, remove the following line.
+  agent.fetchRootKey();
+  let NFTActor;
+
+  async function loadNFT() {
+    NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+
+    const name = await NFTActor.getName();
+    const owner = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(
+        new Blob([imageContent.buffer], { type: "image/png" })
+    );
+
+    setName(name);
+    setOwner(owner.toText());
+    setImage(image);
+
+    if (props.role == "collection") {
+      const nftIsListed = await seesset.isListed(props.id);
+
+      if (nftIsListed) {
+        setOwner("seesset");
+        setBlur({ filter: "blur(4px)" });
+        setSellStatus("Listed");
+      } else {
+        setButton(<Button handleClick={handleSell} text={"Sell"} />);
+      }
+    } else if (props.role == "discover") {
+      const originalOwner = await seesset.getOriginalOwner(props.id);
+      if (originalOwner.toText() != CURRENT_USER_ID.toText()) {
+        setButton(<Button handleClick={handleBuy} text={"Buy"} />);
+      }
+
+      const price = await seesset.getListedNFTPrice(props.id);
+      setPriceLabel(<PriceLabel sellPrice={price.toString()} />);
+    }
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
+  let price;
+  function handleSell() {
+    console.log("Sell clicked");
+    setPriceInput(
+      <input
+        placeholder="Price in ICP"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e) => (price = e.target.value)}
+      />
+    );
+    setButton(<Button handleClick={sellItem} text={"Confirm"} />);
+  }
+
+  async function sellItem() {
+    setBlur({ filter: "blur(4px)" });
+    setLoaderHidden(false);
+    console.log("set price = " + price);
+    const listingResult = await seesset.listItem(props.id, Number(price));
+    console.log("listing: " + listingResult);
+    if (listingResult == "Success") {
+      const SeessetId = await seesset.getSeessetCanisterID();
+      const transferResult = await NFTActor.transferOwnership(SeessetId);
+      console.log("transfer: " + transferResult);
+      if (transferResult == "Success") {
+        setLoaderHidden(true);
+        setButton();
+        setPriceInput();
+        setOwner("seesset");
+        setSellStatus("Listed");
+      }
+    }
+  }
+
+  async function handleBuy() {
+    console.log("Buy was triggered");
+    setLoaderHidden(false);
+    const tokenActor = await Actor.createActor(tokenIdlFactory, {
+      agent,
+      canisterId: Principal.fromText("<REPLACE WITH YOUR TOKEN CANISTER ID>"),
+    });
+
+    const sellerId = await seesset.getOriginalOwner(props.id);
+    const itemPrice = await seesset.getListedNFTPrice(props.id);
+
+    const result = await tokenActor.transfer(sellerId, itemPrice);
+    if (result == "Success") {
+      const transferResult = await seesset.completePurchase(
+        props.id,
+        sellerId,
+        CURRENT_USER_ID
+      );
+      console.log("purchase: " + transferResult);
+      setLoaderHidden(true);
+      setDisplay(false);
+    }
+  }
+
+  return (
+    <div
+      style={{ display: shouldDisplay ? "inline" : "none" }}
+      className="disGrid-item"
+    >
+      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        <img
+          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
+          src={image}
+          style={blur}
+        />
+        <div className="lds-ellipsis" hidden={loaderHidden}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="disCardContent-root">
+          {priceLabel}
+          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
+            {name}
+            <span className="purple-text"> {sellStatus}</span>
+          </h2>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+            Owner: {owner}
+          </p>
+          {priceInput}
+          {button}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
